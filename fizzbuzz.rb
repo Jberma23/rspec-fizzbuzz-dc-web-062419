3a597be79ef15944db1b553850c04e3ca4054ef9
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-12 15:12:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(any)
-  if any % 3 == 0 && any % 5 == 0
-      val="FizzBuzz"
-    elsif any % 3 == 0
-        val="Fizz"
-      elsif any % 5 == 0
-          val="Buzz"
-    else
-      puts "Not divisible by 3 or 5"
-  end 
-
- endt divisible by 3 or 5"
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif int % 3 == 0
+    "Fizz"
+  else nil
+  end
 end
